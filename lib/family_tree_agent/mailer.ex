3939fd135312b6e684c8c18cbfd88c9b86a67815
Juan Rivillas <juan@appcues.com>
2025-07-18 13:13:05 -0300

    Init phoenix API

---
+++ lib/family_tree_agent/mailer.ex
@@ -0,0 +1,3 @@
+defmodule FamilyTreeAgent.Mailer do
+  use Swoosh.Mailer, otp_app: :family_tree_agent
+end
---
+++ test/family_tree_agent_web/controllers/error_html_test.exs
@@ -0,0 +1,14 @@
+defmodule FamilyTreeAgentWeb.ErrorHTMLTest do
+  use FamilyTreeAgentWeb.ConnCase, async: true
+
+  # Bring render_to_string/4 for testing custom views
+  import Phoenix.Template
+
+  test "renders 404.html" do
+    assert render_to_string(FamilyTreeAgentWeb.ErrorHTML, "404", "html", []) == "Not Found"
+  end
+
+  test "renders 500.html" do
+    assert render_to_string(FamilyTreeAgentWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+  end
+end
